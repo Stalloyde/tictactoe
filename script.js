9ad25d374cf4b064890e16bc1402e015ef0c505e
Stalloyde <stalloyde93@gmail.com>
2022-09-28 09:32:46 +0800

    Add full functioning grids & newGame button

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
+let currentPlayer = "";
+const caption = document.querySelector("p");
+const newGame = document.querySelector("button");
+
 //create players using factory functions
-const createPlayer = (name, playerNumber, marking) => {
-    return {name, playerNumber, marking};
+const createPlayer = (name, playerNumber) => {
+    return {name, playerNumber};
 }
 
-const p1 = createPlayer("Bob", "Player 1", "X");
-const p2 = createPlayer("Janice", "Player 2", "O");
+const p1 = createPlayer("Bob", "Player 1");
+const p2 = createPlayer("Janice", "Player 2");
 
 //create an array of gameBoard
 const gameBoardModule = (function () {
-    const gameBoard = [
-        p1.marking, p1.marking, p2.marking,
-        p1.marking, p2.marking, p2.marking,
-        p2.marking, p1.marking, p2.marking,
-    ];
+    const gameBoard = [];
     return {gameBoard};
 })();
 
-(function renderGameBoard () {
+const renderGameBoard = (() => {
+    currentPlayer = p1;
     const array = gameBoardModule.gameBoard;
     const gameGrid = document.querySelectorAll(".game-grid");
-
-    gameGrid.forEach(item => item.addEventListener("click", addMark));      
-
-    function addMark () {
-        for (let i=0; i<array.length; i++) {
-            if ((gameGrid[i].id === this.id) && (array[i] === "X")) {
-                gameGrid[i].innerHTML = "<img src=/home/stalloyde/repos/tictactoe/close_FILL0_wght700_GRAD0_opsz48.svg height=190 width=190>";
-            } else if 
-            ((gameGrid[i].id === this.id) && (array[i] === "O")) {
-                gameGrid[i].innerHTML = "<img src=/home/stalloyde/repos/tictactoe/circle_FILL0_wght700_GRAD0_opsz48.svg height=150 width=150>";
-            }
+    
+    (function addMark () {
+        gameGrid.forEach(item => item.addEventListener("click", addMark));      
+        if (currentPlayer === p1) { 
+            this.innerHTML = "<img src=/home/stalloyde/repos/tictactoe/close_FILL0_wght700_GRAD0_opsz48.svg height=190 width=190>";
+            caption.textContent = `Player 2's turn`;
+            currentPlayer = p2;
+            this.onclick = this.removeEventListener("click", addMark);
+        } else {
+            this.innerHTML = "<img src=/home/stalloyde/repos/tictactoe/circle_FILL0_wght700_GRAD0_opsz48.svg height=150 width=150>";
+            currentPlayer = p1;
+            caption.textContent = `Player 1's turn`
+            this.onclick = this.removeEventListener("click", addMark);
         };
-    };
+    })();
+    //push data into gameBoard array after each click
+ 
+    newGame.addEventListener("click", reset);
+    function reset () {
+        renderGameBoard.gameGrid.forEach(item => item.innerHTML = "");
+    }
+    return {array, gameGrid}
 })();
+
+
+
+
