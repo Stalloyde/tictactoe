d9ed74b7d234952f259cf2dccf49d910736931ce
Stalloyde <stalloyde93@gmail.com>
2022-09-28 10:30:34 +0800

    Fix wrong player turn after reset

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let currentPlayer = "";
 const caption = document.querySelector("p");
 const newGame = document.querySelector("button");
 
@@ -16,34 +15,43 @@ const gameBoardModule = (function () {
     return {gameBoard};
 })();
 
+
+
 const renderGameBoard = (() => {
-    currentPlayer = p1;
     const array = gameBoardModule.gameBoard;
     const gameGrid = document.querySelectorAll(".game-grid");
+    let currentPlayer = p1;
+    gameGrid.forEach(item => item.addEventListener("click", addMark));      
     
-    (function addMark () {
-        gameGrid.forEach(item => item.addEventListener("click", addMark));      
+    function addMark () {
         if (currentPlayer === p1) { 
             this.innerHTML = "<img src=/home/stalloyde/repos/tictactoe/close_FILL0_wght700_GRAD0_opsz48.svg height=190 width=190>";
             caption.textContent = `Player 2's turn`;
+            this.onclick = this.removeEventListener("click", addMark);
             currentPlayer = p2;
-            this.onclick = this.removeEventListener("click", addMark);
-        } else {
-            this.innerHTML = "<img src=/home/stalloyde/repos/tictactoe/circle_FILL0_wght700_GRAD0_opsz48.svg height=150 width=150>";
-            currentPlayer = p1;
-            caption.textContent = `Player 1's turn`
-            this.onclick = this.removeEventListener("click", addMark);
-        };
-    })();
-    //push data into gameBoard array after each click
- 
+        } else if
+            (currentPlayer === p2) {
+                this.innerHTML = "<img src=/home/stalloyde/repos/tictactoe/circle_FILL0_wght700_GRAD0_opsz48.svg height=150 width=150>";
+                caption.textContent = `Player 1's turn`
+                this.onclick = this.removeEventListener("click", addMark);
+                currentPlayer = p1;
+
+            };
+        //push data into gameBoard array after each click
+    };
+    
     newGame.addEventListener("click", reset);
+        
     function reset () {
-        renderGameBoard.gameGrid.forEach(item => item.innerHTML = "");
-    }
-    return {array, gameGrid}
+        gameGrid.forEach(item => item.innerHTML = "");
+        currentPlayer = p1; 
+        caption.textContent = `Player 1's turn`;
+        gameGrid.forEach(item => item.addEventListener("click", addMark));    
+    };
+
+    return {array, gameGrid, currentPlayer, addMark}
 })();
 
 
-
+    
 
