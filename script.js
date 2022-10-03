81f695e0c96ed16c2bcb7608bd15e46c5133d7dd
Stalloyde <stalloyde93@gmail.com>
2022-10-03 21:51:14 +0800

    Complete base game (PvP)

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const caption = document.getElementById("caption");
-const newGame = document.querySelector("button");
 const p1Score = document.getElementById("p1Score");
 const p2Score = document.getElementById("p2Score");
+const p1Input = document.getElementById("p1-name");
+const p2Input = document.getElementById("p2-name");
+const newGame = document.querySelector(".new-round");
+const startGameBtn = document.querySelector(".start-button");
+const startGame = document.querySelector(".start-button");
+const form = document.querySelector(".form");
+let p1Name = "";
+let p2Name = "";
 
-//create players using factory functions
-const createPlayer = (name, playerNumber, marking) => {
-    return {name, playerNumber, marking};
-}
+startGame.addEventListener("click", () => {
+    form.style.display ="none";
+    p1Name = p1Input.value;
+    p2Name = p2Input.value;
+    renderGameBoard();
+});
 
-const p1 = createPlayer("Bob", "Player 1", "X");
-const p2 = createPlayer("Janice", "Player 2", "O");
+
 
 //create an array of gameBoard
 const gameBoardModule = (function () {
@@ -21,14 +29,24 @@ const gameBoardModule = (function () {
 const arrayP1 = gameBoardModule.gameBoardP1;
 const arrayP2 = gameBoardModule.gameBoardP2;
 
-const renderGameBoard = (() => {  
+const renderGameBoard = () => {  
+    //create players using factory functions
+    const createPlayer = (name, playerNumber, marking) => {
+        return {name, playerNumber, marking};
+    }
+
+    const p1 = createPlayer(p1Name, "Player 1", "X");
+    const p2 = createPlayer(p2Name, "Player 2", "O");
+
     const gameGrid = document.querySelectorAll(".game-grid");
     let currentPlayer = p1;
     gameGrid.forEach(item => item.addEventListener("click", addMark));      
     gameGrid.forEach(item => item.addEventListener("mouseover", function () {
         if (currentPlayer === p1) {
+            console.log(p1)
             item.style.backgroundColor = "rgb(255, 103, 231)";
         } else {
+            console.log(p2)
             item.style.backgroundColor = "rgb(12, 236, 221)";
         }
     }));
@@ -37,26 +55,26 @@ const renderGameBoard = (() => {
         item.style.backgroundColor = "black";
     }))
     
-    function playerTurn (player) {
+    function appendPlayerTurn (player) {
         caption.textContent = `${player.name}'s (${player.playerNumber}) Turn`;
     }
-
+    
     function addMark () {
         if (currentPlayer === p1) { 
             this.innerHTML = "<img src=cross.svg height=150 width=150>";
             this.onclick = this.removeEventListener("click", addMark);
             currentPlayer = p2;
-            playerTurn(p2);
+            appendPlayerTurn(p2);
             arrayP1.push(Number(this.id));
         } else if
-            (currentPlayer === p2) {
-                this.innerHTML = "<img src=circle.svg height=120 width=120>";
-                this.onclick = this.removeEventListener("click", addMark);
-                currentPlayer = p1;
-                playerTurn(p1);
-                arrayP2.push(Number(this.id));
-            };
-
+        (currentPlayer === p2) {
+            this.innerHTML = "<img src=circle.svg height=120 width=120>";
+            this.onclick = this.removeEventListener("click", addMark);
+            currentPlayer = p1;           
+            appendPlayerTurn(p1);
+            arrayP2.push(Number(this.id));
+        };
+        
         (function determineWinner () {
             const threeInARow = {
                 1: [0,1,2],
@@ -68,7 +86,7 @@ const renderGameBoard = (() => {
                 7: [6,7,8],
                 8: [2,4,6],
             };
-
+            
             function endRound (player) {
                 gameGrid.forEach(item => item.removeEventListener("click", addMark));
                 caption.textContent = `${player.name} (${player.playerNumber}) Wins!`;
@@ -100,16 +118,16 @@ const renderGameBoard = (() => {
         arrayP1.splice(0, arrayP1.length);
         arrayP2.splice(0, arrayP2.length);
         currentPlayer = p1; 
-        playerTurn(p1);
+        appendPlayerTurn(p1);
         gameGrid.forEach(item => item.addEventListener("click", addMark));    
     };
-
-    return {arrayP1, arrayP2, gameGrid, currentPlayer, addMark}
-})();
-
-
-
-
-
     
+    return {arrayP1, arrayP2, gameGrid, currentPlayer, addMark}
+
+};
+
+
+
+
+
 
